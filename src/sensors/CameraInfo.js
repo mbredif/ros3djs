00846ddbbb465f7ac1b1782f4ed873c8c7927728
Mathieu Brédif <mathieu.bredif@ign.fr>
2018-04-10 19:10:06 +0200

    camerainfo pyramids

--- src/sensors/CameraInfo.js
+++ src/sensors/CameraInfo.js
@@ -1,5 +1,5 @@
 /**
- * @author Mathieu Bredif!! - mathieu.bredif@ign.fr
+ * @author Mathieu Bredif - mathieu.bredif@ign.fr
  */
 
 /**
@@ -13,20 +13,33 @@
  *  * tfClient - the TF client handle to use
  *  * rootObject (optional) - the root object to add this marker to
  *  * color (optional) - color for line (default: 0xcc00ff)
- *  * length (optional) - the length of the arrow (default: 1.0)
- *  * headLength (optional) - the head length of the arrow (default: 0.2)
- *  * shaftDiameter (optional) - the shaft diameter of the arrow (default: 0.05)
- *  * headDiameter (optional) - the head diameter of the arrow (default: 0.1)
+ *  * height (optional) - the height of the image pyramid (default: 1.0)
  */
 ROS3D.CameraInfo = function(options) {
   this.options = options || {};
   this.ros = options.ros;
-  this.topicName = options.topic || '/pose';
+  this.topicName = options.topic || '/camera_info';
   this.tfClient = options.tfClient;
   this.color = options.color || 0xcc00ff;
   this.rootObject = options.rootObject || new THREE.Object3D();
   THREE.Object3D.call(this);
-
+  
+  this.geom = new THREE.BufferGeometry();
+  this.vertices = new THREE.BufferAttribute(new Float32Array( 3 * 5 ), 3 );
+  this.geom.addAttribute( 'position',  this.vertices);
+  this.geom.setIndex( [ 0,1,2, 0,2,3, 0,3,4, 0,4,1, 3,2,1, 1,4,3] );
+  this.material = new THREE.MeshBasicMaterial( { wireframe: false, color: this.color } );
+  this.object = new THREE.Mesh( this.geom, this.material );
+  this.setHeight(options.height || 0.1);
+  
+  // this.geom.setDrawRange(0,12); // cone only
+  // this.geom.setDrawRange(12,6); // base only
+  
+  this.vertices.array[ 5] = 1;
+  this.vertices.array[ 8] = 1;
+  this.vertices.array[11] = 1;
+  this.vertices.array[14] = 1;
+  
   this.sn = null;
 
   this.rosTopic = undefined;
@@ -34,6 +47,11 @@ ROS3D.CameraInfo = function(options) {
 };
 ROS3D.CameraInfo.prototype.__proto__ = THREE.Object3D.prototype;
 
+ROS3D.CameraInfo.prototype.setHeight = function(height){
+  this.height = height;
+  this.object.scale.set(height, height, height);
+  // this.object.matrixWorldNeedsUpdate = true; // not necessary ?
+}
 
 ROS3D.CameraInfo.prototype.unsubscribe = function(){
   if(this.rosTopic){
@@ -54,26 +72,38 @@ ROS3D.CameraInfo.prototype.subscribe = function(){
 };
 
 ROS3D.CameraInfo.prototype.processMessage = function(message){
-  if(this.sn!==null){
-      this.sn.unsubscribeTf();
-      this.rootObject.remove(this.sn);
-  }
-
-  this.options.origin = new THREE.Vector3( message.pose.position.x, message.pose.position.y,
-                                           message.pose.position.z);
-
-  var rot = new THREE.Quaternion(message.pose.orientation.x, message.pose.orientation.y,
-                                 message.pose.orientation.z, message.pose.orientation.w);
-  this.options.direction = new THREE.Vector3(1,0,0);
-  this.options.direction.applyQuaternion(rot);
-  this.options.material = new THREE.MeshBasicMaterial({color: this.color});
-  var arrow = new ROS3D.Arrow(this.options);
-
-  this.sn = new ROS3D.SceneNode({
+  if(this.sn===null){
+    this.sn = new ROS3D.SceneNode({
       frameID : message.header.frame_id,
       tfClient : this.tfClient,
-      object : arrow
-  });
+      object : this.object
+    });
+    this.rootObject.add(this.sn);
+  }
+  var fx = message.K[0];
+  var fy = message.K[4];
+  var px = message.K[2];
+  var py = message.K[5];
+  var s  = message.K[1];
+  var w  = message.width;
+  var h  = message.height;
 
-  this.rootObject.add(this.sn);
+  var y0 = -py/fy;
+  var y1 = (h-py)/fy;
+  var x0 = -px/fx;
+  var x1 = (w-px)/fx;
+  var sfx = s/fx;
+  var s0 = y0*sfx;
+  var s1 = y1*sfx;
+  
+  this.vertices.array[ 3] = x1-s1;
+  this.vertices.array[ 4] = y1;
+  this.vertices.array[ 6] = x0-s1;
+  this.vertices.array[ 7] = y1;
+  this.vertices.array[ 9] = x0-s0;
+  this.vertices.array[10] = y0;
+  this.vertices.array[12] = x1-s0;
+  this.vertices.array[13] = y0;
+  
+  this.vertices.needsUpdate = true;
 };
